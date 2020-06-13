c7f5b9bf4046970048a8a2250951c32806abe719
tasankuluulu <tasankuluulu@gmail.com>
2020-06-13 10:30:33 -0400

    no comments 0613 1030

---
+++ src/com/hackerrank/problems/ArratCounterOtherWay.java
@@ -0,0 +1,21 @@
+package com.hackerrank.problems;
+
+import java.util.Scanner;
+
+public class ArratCounterOtherWay {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int m = in.nextInt();
+		int n = in.nextInt();
+		int x;
+		int counterList[] = new int[n];
+
+		for (int i = 0; i < m; i++) {
+			x = in.nextInt();
+			counterList[(x - 1)] += 1;
+		}
+		for (int i : counterList) {
+			System.out.print(i + " ");
+		}
+	}
+}
