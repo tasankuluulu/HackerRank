989a3b8e30c854edadaa41ee48d112b3bf9d9586
tasankuluulu <tasankuluulu@gmail.com>
2020-06-29 10:01:15 -0400

    update from office

---
+++ src/com/hackerrank/problems/MultipleClasses.java
@@ -0,0 +1,10 @@
+package com.hackerrank.problems;
+
+public class MultipleClasses {
+	public static void main(String[] args) {
+		Tuna tunaObj = new Tuna();
+		tunaObj.simpleMessage();
+		
+	}
+
+}
---
+++ src/com/hackerrank/problems/Tuna.java
@@ -0,0 +1,8 @@
+package com.hackerrank.problems;
+
+public class Tuna {
+	public void simpleMessage() {
+		System.out.println("Hi!!!");
+	}
+
+}
