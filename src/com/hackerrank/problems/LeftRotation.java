03fec74511f3f4995759876742d1c65bd4733eed
tasankuluulu <tasankuluulu@gmail.com>
2020-07-07 19:01:15 -0400

    update

---
+++ src/com/hackerrank/problems/LeftRotation.java
@@ -0,0 +1,21 @@
+package com.hackerrank.problems;
+
+import java.util.Scanner;
+
+public class LeftRotation {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[scan.nextInt()];
+		int numRoration = scan.nextInt();
+		
+		for (int i  = 0; i < array.length; i ++) {
+			array[i] = scan.nextInt();
+		}
+		for (int i = numRoration; i < array.length + numRoration; i++) {
+			array[] 
+		}
+		
+		scan.close();
+	}
+
+}
